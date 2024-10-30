69329dbac68d63a206da46078992dcf3c717281f
Jimies22 <2201101389@student.buksu.edu.ph>
2024-10-30 18:01:53 +0800

    Update code in User specifically routes and index.js

--- server/index.js
+++ server/index.js
@@ -1,54 +1,20 @@
-// const express = require("express");
-// const app = express();
-// const PORT = process.env.PORT || 6969;
-
-// //const userRouter = require("./routes/user");
-// const userRouter = require("./routes/User/userRoutes");
-
-// // const requestRouter = require("./routes/User/");
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use("/user", userRouter);
-// app.use("/request", requestRouter);
-
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   console.log(`Request Method: ${req.method}, URL: ${req.url}`);
-//   next();
-// });
-
-// app.use((err, req, res, next) => {
-//   console.error(err);
-//   res.status(500).send("Internal Server Error");
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-const express = require("express"); // Corrected the require statement
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const userRouter = require("./routes/User/userRoute"); // Ensure the path is correct
-const bookingRouter = require("./routes/User/bookingRoutes"); // Ensure the path is correct
-const dashboardRouter = require("./routes/User/dashboardRoute"); // Add the new import
-const inventoryRouter = require("./routes/User/inventoryRoute"); // Add the new import
-const notificationRouter = require("./routes/User/notificationRoute"); // Add the new import
+const authRouter = require("./routes/User/authRoute");
+const userRouter = require("./routes/User/userRoute");
+const bookingRouter = require("./routes/User/bookingRoutes");
+const dashboardRouter = require("./routes/User/dashboardRoute");
+const inventoryRouter = require("./routes/User/inventoryRoute");
+const notificationRouter = require("./routes/User/notificationRoute");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.json({ limit: "50mb" }));
+app.use(
+  express.urlencoded({ extended: true, limit: "50mb", parameterLimit: 10000 })
+);
 
+app.use("/auth", authRouter);
 app.use("/user", userRouter);
 app.use("/booking", bookingRouter);
 app.use("/dashboard", dashboardRouter);
