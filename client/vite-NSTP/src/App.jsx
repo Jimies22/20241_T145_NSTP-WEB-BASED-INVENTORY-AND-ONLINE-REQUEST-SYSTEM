e0ff7bd45b6fb2f11307213e459267f953189f2a
Jimies22 <2201101389@student.buksu.edu.ph>
2024-11-11 22:35:55 +0800

    Add path for user and admin

--- client/vite-NSTP/src/App.jsx
+++ client/vite-NSTP/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/login.jsx";
+//import Login from "./pages/login.jsx";
 
 function App() {
   console.log("App is rendering");
@@ -10,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/user" element={<UserPage />} />
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </Router>
   );
