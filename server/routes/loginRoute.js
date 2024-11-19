dc1e4e696a907f9bdec0cb2d40dca332d7391cd9
Jimies22 <2201101389@student.buksu.edu.ph>
2024-11-19 14:08:43 +0800

    chore: remove unused files and configurations from client and server

--- server/routes/loginRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { googleLogin, verifyRecaptcha } from "../controllers/authController.js";
-
-const router = express.Router();
-
-router.post("/verify-recaptcha", verifyRecaptcha);
-router.post("/google", googleLogin);
-
-export default router;
